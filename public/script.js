d6bb952911ba509880a5ed4e8dbae2109ea0cea4
Ankit <ankitmaurya0225@gmail.com>
2021-08-12 15:19:31 +0530

    deleted data
    Signed-off-by: Ankit <ankitmaurya0225@gmail.com>

--- public/script.js
+++ public/script.js
@@ -1,18 +1,3 @@
 let socket = io()
 
-let btnrequest = document.querySelector('.btn')
-let inpMsg = document.querySelector('.inpMsg')
-let ulmsglist = document.querySelector('.ulmsglist')
 
-btnrequest.onclick = function () {
-    socket.emit('msg_send' , {
-        msg: inpMsg.value
-    })
-    inpMsg.value 
-}
-
-socket.on('recieved', (data) => {
-    let listnewmsg = document.createElement('li')
-    listnewmsg.innerText = data.msg
-    ulmsglist.appendChild(listnewmsg)
-})
