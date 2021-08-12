36102d71f82e00dd9e42acfd97404878b9544caa
Ankit <ankitmaurya0225@gmail.com>
2021-08-12 13:41:17 +0530

    socket broadcasting
    Signed-off-by: Ankit <ankitmaurya0225@gmail.com>

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const app = express()
 const server = http.createServer(app)
 const io = socket(server)
 
+io.on('connection', (socket) => {
+  console.log('connected with socket id =' , socket.id);
+
+  socket.on('msg_send', (data) =>{
+    // console.log('recieved ',data.msg);
+    // io.emit('recieved', data) // sends everybody
+    // socket.emit('recieved', data) // sends to all and visible to you
+    socket.broadcast.emit('recieved',data) // this visibles the message to all the users except you
+  })
+
+})
 app.use('/' , express.static(__dirname + '/public'))
 
 server.listen(4000,() => {
