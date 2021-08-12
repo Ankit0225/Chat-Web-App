141753005b9e60cb58cd4ccc58e2a5f45f13922f
Ankit <ankitmaurya0225@gmail.com>
2021-08-12 17:53:36 +0530

    added usr and personal user

--- public/script.js
+++ public/script.js
@@ -1,3 +1,25 @@
 let socket = io()
 
+$('.loginbox').show()
+$('.chatbox').hide()
 
+$('.start').click(() => {
+    socket.emit('login', {
+        User: $('.inpUsername').val()
+    })
+})
+socket.on('logged in' ,(data) => {
+    $('.loginbox').hide()
+    $('.chatbox').show()
+})
+
+$('.btnRequest').click(() => {
+    socket.emit('msg_send', {
+        to: $('.inpToUser').val(),
+        msg: $('.inpNewMsg').val()
+    })
+})
+
+socket.on('recieved', (data) => {
+    $('.ulmsglist').append($('<li>').text(data.msg))
+})
